decaffa52b153cf3b074295e38dbd5dbbb3120e1
AnatholyB1 <anatholy.bricon@viacesi.fr>
2023-11-04 23:54:32 +0100

    no overflow-y

--- Gecko_Cabane_Restaurant/src/layer.tsx
+++ Gecko_Cabane_Restaurant/src/layer.tsx
@@ -3,7 +3,7 @@ import Footer from "@/static/footer";
 
 export default function Layer({children}: {children? :React.ReactNode}) {
     return(
-        <div className="grid drid-flow-row place-items-center w-screen h-screen overflow-y-auto">
+        <div className="grid drid-flow-row place-items-center w-screen h-screen overflow-y-auto overflow-x-hidden">
             <Header/>
             {children}
             <Footer/>
