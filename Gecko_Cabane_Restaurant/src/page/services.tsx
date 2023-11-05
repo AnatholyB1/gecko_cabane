5de3dfe176dc846fd71e127644fdfe88e36ae237
AnatholyB1 <anatholy.bricon@viacesi.fr>
2023-11-05 09:53:06 +0100

    correct latte and correct css

--- Gecko_Cabane_Restaurant/src/page/services.tsx
+++ Gecko_Cabane_Restaurant/src/page/services.tsx
@@ -3,7 +3,7 @@ import Layer from "@/layer"
 
 export default function Services() {
     return  <Layer>
-                <section className="grid place-items-center grid-flow-row  bg-white py-[5rem] w-full service-page" >
+                <section className="grid place-items-center grid-flow-row  bg-white py-[5rem] w-full service-page " >
                     <h1 className="text-primary text-center text-2xl font-bold leading-10 transition-all duration-300 md:text-4xl hover:scale-150">WE'LL BE HERE SOON!</h1>
                     <OnCreation></OnCreation>
                 </section>
--- Gecko_Cabane_Restaurant/src/page/shop.tsx
+++ Gecko_Cabane_Restaurant/src/page/shop.tsx
@@ -1,13 +1,14 @@
 import Layer from "@/layer"
-
+import Lottie   from "lottie-react"
+import Icon from '@/assets/wired-outline-298-coins.json'
 export default function Shop() {
 
   
 
     return <Layer>
-                <section className="grid place-items-center grid-flow-col  bg-white py-[5rem] w-full service-page" >
+                <section className="grid place-items-center grid-flow-row  bg-white py-[5rem] w-full h-auto " >
                     <h1 className="text-primary text-center text-2xl font-bold leading-10 transition-font-size duration-300 md:text-4xl">SHOP IN CUSTOMISATION!</h1>
-
+                    <Lottie  className="w-full h-full" animationData={Icon}></Lottie>
                 </section>
            </Layer>
 }
