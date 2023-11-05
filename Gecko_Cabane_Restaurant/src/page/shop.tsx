47fff015461f3560af2e16ff3bfface7edb05b1c
AnatholyB1 <anatholy.bricon@viacesi.fr>
2023-11-05 08:45:26 +0100

    undo lord icons

--- Gecko_Cabane_Restaurant/src/page/shop.tsx
+++ Gecko_Cabane_Restaurant/src/page/shop.tsx
@@ -1,9 +1,13 @@
 import Layer from "@/layer"
 
 export default function Shop() {
+
+  
+
     return <Layer>
                 <section className="grid place-items-center grid-flow-col  bg-white py-[5rem] w-full service-page" >
                     <h1 className="text-primary text-center text-2xl font-bold leading-10 transition-font-size duration-300 md:text-4xl">SHOP IN CUSTOMISATION!</h1>
+
                 </section>
            </Layer>
 }
