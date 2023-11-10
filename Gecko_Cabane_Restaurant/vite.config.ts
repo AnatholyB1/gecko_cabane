331c26e7d720fe425585255668c0efaab42fddac
AnatholyB1 <anatholy.bricon@viacesi.fr>
2023-11-10 19:13:30 +0100

    regler le probleme en prod

--- Gecko_Cabane_Restaurant/vite.config.ts
+++ Gecko_Cabane_Restaurant/vite.config.ts
@@ -3,15 +3,20 @@ import react from '@vitejs/plugin-react'
 import path from "path";
 import { imagetools } from 'vite-imagetools'
 import { compression } from 'vite-plugin-compression2'
-
+import { VitePWA } from 'vite-plugin-pwa'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), 
     imagetools(), 
-    compression({ algorithm: 'brotliCompress', exclude: [/\.(br)$/, /\.(gz)$/], deleteOriginalAssets: true }), 
-   ],
+    compression({ algorithm: 'brotliCompress', exclude: [/\.(br)$/, /\.(gz)$/]}), 
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      }
+    })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
