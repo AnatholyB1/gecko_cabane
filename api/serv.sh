8d1e2f08436607c4cb4ac27af9a130166e032452
AnatholyB1 <anatholy.bricon@viacesi.fr>
2023-11-12 13:24:21 +0100

    update for build

--- api/serv.sh
+++ api/serv.sh
@@ -4,4 +4,5 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install 18
-node dist/index.js
+npm install 
+tsc
