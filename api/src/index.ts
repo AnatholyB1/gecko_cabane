d62a3d066634e8b1af9d8a02e8befbbad7598f95
AnatholyB1 <anatholy.bricon@viacesi.fr>
2023-11-12 11:22:07 +0100

    api ready for prod

--- api/src/index.ts
+++ api/src/index.ts
@@ -6,19 +6,31 @@ import compression from 'compression';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import router from './router';
+import dotenv from 'dotenv';
+import path from 'path';
+
+
 const app = express();	// Création de l'application express
 
 app.use(compression());	// Compression des requêtes 
 app.use(cors({
+    origin: ['http://localhost:5173', 'https://gecko-cabane.vercel.app'],
     credentials: true,
 }));	// Autorisation des requêtes cross-domain
 app.use(boydParser.json());	// Parser les requêtes en JSON
 app.use(cookieParser());	// Parser les cookies
 
 const server = http.createServer(app);	// Création du serveur HTTP
-import dotenv from 'dotenv';
-dotenv.config();
-const MONGO_URL = process.env.MONGO_CONNECTION || 'mongodb://localhost:27017/express-mongo';	// Connexion à la base de données MongoDB
+
+dotenv.config ({path: path.resolve(__dirname, "../.env") });
+const MONGO_URL = process.env.MONGO_CONNECT 	// Connexion à la base de données MongoDB
+
+
+
+
+
+
+
 
 server.listen(8080, () => {
     console.log('Server running on port 8080');
@@ -26,7 +38,7 @@ server.listen(8080, () => {
 
 
 mongoose.Promise = Promise;
-mongoose.connect(MONGO_URL);
+mongoose.connect(MONGO_URL ?? '');
 mongoose.connection.on('error', err => {
     console.error(`MongoDB connection error: ${err}`);
     process.exit(1);
